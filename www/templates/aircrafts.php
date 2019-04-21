4aa49681688e0552cacafb6927a6d136e0b2c24f
yugecin <yugecin@users.noreply.github.com>
2019-04-21 04:58:49 +0200

    add vehicle model name in flight table

---
+++ www/templates/aircrafts.php
@@ -0,0 +1,28 @@
+<?php 
+function aircraft_name($model)
+{
+	static $names = array(
+		'417'=>'Leviathan',
+		'425'=>'Hunter',
+		'460'=>'Skimmer',
+		'467'=>'Rustler',
+		'469'=>'Sparrow',
+		'487'=>'Maverick',
+		'488'=>'News Chopper',
+		'497'=>'Police Maverick',
+		'511'=>'Beagle',
+		'519'=>'Shamal',
+		'520'=>'Hydra',
+		'548'=>'Cargobob',
+		'553'=>'Nevada',
+		'563'=>'Raindance',
+		'577'=>'AT-400',
+		'592'=>'Andromada',
+		'593'=>'Dodo',
+	);
+	if (array_key_exists($model, $names)) {
+		return $names[$model];
+	}
+	return 'Unknown';
+}
+?>
