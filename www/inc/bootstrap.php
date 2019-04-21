8955be3b2c6642bc31cd0a248aec49c3dadeb74c
yugecin <yugecin@users.noreply.github.com>
2019-04-21 02:38:10 +0200

    page with flights overview

--- www/inc/bootstrap.php
+++ www/inc/bootstrap.php
@@ -6,3 +6,18 @@ $__msgs = [];
 require('inc/conf.php');
 include('inc/db.php');
 
+function simple_pagination($urlWithParam, $currentPageFromOne, $totalPages)
+{
+	$str = '<p class="pagination">Page:';
+	for ($i = 0; $i < $totalPages; ) {
+		$i++;
+		if ($i == $currentPageFromOne) {
+			$str .= ' <b>' . $i . '</b>';
+		} else {
+			$str .= " <a href=\"{$urlWithParam}{$i}\">{$i}</a>";
+		}
+	}
+	$str .= '</p>';
+	return $str;
+}
+
---
+++ www/templates/flightstatuses.php
@@ -0,0 +1,11 @@
+<?php 
+$flightstatuses = array(
+	'1'=>'In progress',
+	'2'=>'Abandoned',
+	'4'=>'Crashed',
+	'8'=>'Finished',
+	'16'=>'Declined',
+	'32'=>'Died',
+	'64'=>'Server error'
+);
+?>
