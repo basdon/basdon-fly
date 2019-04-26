5b900fa2cd77c5a04864ed0dae94fd55361be6e9
yugecin <yugecin@users.noreply.github.com>
2019-04-26 23:07:08 +0200

    update genpages cli script for new directory structure

--- cli/genpages.php
+++ cli/genpages.php
@@ -2,7 +2,7 @@
 
 chdir('../www');
 
-require('inc/spate.php');
+require('../inc/spate.php');
 spate_default_generate('index');
 spate_default_generate('flights');
 spate_default_generate('flight');
