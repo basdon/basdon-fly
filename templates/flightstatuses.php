8f1e2ab942c2ae2205d903a82a9f4ed0da81cedd
yugecin <yugecin@users.noreply.github.com>
2019-04-27 23:43:25 +0200

    basic flight info on flight page

--- templates/flightstatuses.php
+++ templates/flightstatuses.php
@@ -8,4 +8,15 @@ $flightstatuses = array(
 	'32'=>'Died',
 	'64'=>'Server error'
 );
+function fmt_flight_status($state, $tload)
+{
+	global $flightstatuses;
+	if ($state == 1 && $tload == 0) {
+		return 'Started';
+	}
+	if (array_key_exists($state, $flightstatuses)) {
+		return $flightstatuses[$state];
+	}
+	return 'Unknown?';
+}
 ?>
