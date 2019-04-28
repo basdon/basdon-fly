cb93cc3930734c8cd6a74133ca00f20272cc6679
yugecin <yugecin@users.noreply.github.com>
2019-04-28 15:37:04 +0200

    finish flight page with all details and payment info

---
+++ templates/missiontypes.php
@@ -0,0 +1,22 @@
+<?php 
+function fmt_mission_type($missiontype)
+{
+	switch ($missiontype) {
+	case 1: return 'Passengers (small quantity)';
+	case 2: return 'Passengers (medium quantity)';
+	case 4: return 'Passengers (large quantity)';
+	case 8: return 'Cargo';
+	case 16: return 'Cargo';
+	case 32: return 'Cargo';
+	case 64: return 'Helicopter (passengers)';
+	case 128: return 'Helicopter (cargo)';
+	case 256: return 'Helicopter (military)';
+	case 512: return 'Military';
+	case 1024: return 'AWACS';
+	case 2048: return 'Stunt';
+	case 4096: return 'Cropduster';
+	case 8192: return 'Passengers (small quantity, water)';
+	default: return 'Unknown';
+	}
+}
+?>
