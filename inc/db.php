86ea5494cf4682461925dee0f008b5d1ff0c417d
yugecin <yugecin@users.noreply.github.com>
2019-08-03 22:49:57 +0200

    use utf8mb4 for mysql

--- inc/db.php
+++ inc/db.php
@@ -7,7 +7,7 @@ try {
 	$db = new PDO("mysql:host={$db_host};dbname={$db_name}", $db_uname, $db_passw);
 	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$db->exec('SET NAMES utf8');
+	$db->exec('SET NAMES utf8mb4');
 	++$db_querycount;
 } catch (PDOException $e) {
 	$__msgs[] = "Cannot connect to db.";
