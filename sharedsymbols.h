4a927550ce95249cc9f99f9e1e8521da35c43355
yugecin <yugecin@users.noreply.github.com>
2019-08-29 21:28:38 +0200

    add colors to sharedsymbols (previously in gm/colors.pwn)

--- sharedsymbols.h
+++ sharedsymbols.h
@@ -31,6 +31,23 @@
 #define INFO "* "
 #define SUCC "+ "
 
+#define COL_SAMP_GREEN 0x88aa62ff // as in username error message
+#define COL_SAMP_GREY 0xa9c4e4ff // as in 'Connected to..'
+#define COL_SAMP_BLUE 0x2587ceff // as in '* Admin:'
+#define COL_INFO 0x3498dbff
+#define COL_WARN 0xe84c3dff
+#define COL_SUCC 0x2cc36bff
+#define COL_INFO_GENERIC 0xffffffff
+#define COL_INFO_LIGHT 0x33ccffff
+#define COL_PRIVMSG 0xffff00ff
+#define COL_PRIVMSG_HINT 0xafafafff
+#define COL_MISSION 0xff9900ff
+
+#define COL_JOIN 0x1b8ae4ff
+#define COL_QUIT 0x1674Bcff
+
+#define COLOR_METAR COL_INFO_LIGHT
+
 /* airport.c / airport.pwn */
 #define MAX_AIRPORT_NAME (24)
 
