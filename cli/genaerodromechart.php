0e0c690dc6d3b456bb3013f552b949732e44cc70
yugecin <yugecin@users.noreply.github.com>
2019-09-08 01:45:32 +0200

    don't print vehicle spawns and mission point info in aerodrome

--- cli/genaerodromechart.php
+++ cli/genaerodromechart.php
@@ -69,11 +69,10 @@ $miny -= 50;
 $imgw = 500;
 $sizex = $maxx - $minx;
 $sizey = $maxy - $miny;
-$scale = ($imgw - 100) / $sizex;
+$scale = $imgw / $sizex;
 $imgh = $sizey * $scale;
 $offx = -$minx;
 $offy = -$miny;
-$offx += 100 / $scale;
 
 $PI2 = pi() / 2;
 $PI4 = pi() / 4;
@@ -90,6 +89,7 @@ $color_msp = imagecolorallocate($im, 170, 0, 0);
 $color_msp_outline = $color_black;
 $font = 2;
 
+$runway_fillings = [];
 foreach ($runway_ends as $r) {
 	if (count($r) == 2) {
 		$ox = (int) xcoord($r[0]->x);
@@ -118,7 +118,7 @@ foreach ($runway_ends as $r) {
 			$oy - $w * sin($angle + $PI4),
 			$ox - $w * cos($angle - $PI4),
 			$oy - $w * sin($angle - $PI4)];
-		imagefilledpolygon($im, $pts, 4, $bg);
+		$runway_fillings[] = $pts;
 	} else {
 		// helipad
 		$r = $r[0];
@@ -133,6 +133,11 @@ foreach ($runway_ends as $r) {
 	}
 }
 
+foreach ($runway_fillings as $rf)
+{
+	imagefilledpolygon($im, $rf, 4, $bg);
+}
+
 $mspsize = 5;
 $vehsize = 3;
 
@@ -145,56 +150,15 @@ function rect($x, $y, $size, $col_outline, $col)
 	imagefilledrectangle($im, $x - $ss, $y - $ss, $x + $ss, $y + $ss, $col);
 }
 
-$missiontypes = ['cargo' => 0, 'passengers' => 0, 'heli' => 0, 'military' => 0, 'military heli' => 0, 'other' => 0];
 foreach ($missionpoints as $p) {
 	$x = xcoord($p->x);
 	$y = ycoord($p->y);
 	rect($x, $y, $mspsize, $color_msp_outline, $color_msp);
-	if ($p->t & (1 | 2 | 4)) {
-		$missiontypes['passengers']++;
-	} else if ($p->t & (8 | 16 | 32)) {
-		$missiontypes['cargo']++;
-	} else if ($p->t & (64 | 128)) {
-		$missiontypes['heli']++;
-	} else if ($p->t & (256)) {
-		$missiontypes['military']++;
-	} else if ($p->t & (512)) {
-		$missiontypes['military heli']++;
-	} else {
-		$missiontypes['other']++;
-	}
 }
 
-$vehnames = [
-	592 => 'Andromada',
-	577 => 'AT-400',
-	511 => 'Beagle',
-	548 => 'Cargobob',
-	512 => 'Crop duster',
-	593 => 'Dodo',
-	425 => 'Hunter',
-	520 => 'Hydra',
-	417 => 'Leviathan',
-	487 => 'Maverick',
-	553 => 'Nevada',
-	497 => 'Police maverick',
-	563 => 'Raindance',
-	476 => 'Rustler',
-	488 => 'San News heli',
-	447 => 'Seasparrow',
-	519 => 'Shamal',
-	460 => 'Skimmer',
-	469 => 'Sparrow',
-	513 => 'Stuntplane'
-];
-$vehs = [];
-foreach ($vehnames as $k => $v) {
-	$vehs[$k] = 0;
-}
 foreach ($vehicles as $v) {
 	$x = xcoord($v->x);
 	$y = ycoord($v->y);
-	$vehs[$v->m]++;
 	rect($x, $y, $vehsize, $color_veh_outline, $color_veh);
 }
 
@@ -227,29 +191,6 @@ foreach ($servicepoints as $svp) {
 	imagesetpixel($im, $x + 7, $y + 6, $color_black);
 	imagesetpixel($im, $x + 4, $y + 9, $color_black);
 	imagesetpixel($im, $x + 5, $y + 9, $color_black);
-}
-
-$y = 2;
-$fh = imagefontheight($font);
-if (array_sum($missiontypes) != 0) {
-	imagestring($im, $font, 2, $y, "Mission points:", $color_black);
-	$y += $fh;
-	foreach ($missiontypes as $type => $amount) {
-		if ($amount > 0) {
-			imagestring($im, $font, 2, $y, " $type ($amount)", $color_black);
-			$y += $fh;
-		}
-	}
-}
-if (array_sum($vehs) != 0) {
-	imagestring($im, $font, 2, $y, "Vehicles:", $color_black);
-	$y += $fh;
-	foreach ($vehs as $id => $amount) {
-		if ($amount > 0) {
-			imagestring($im, $font, 2, $y, " {$vehnames[$id]} ($amount)", $color_black);
-			$y += $fh;
-		}
-	}
 }
 
 if (!isset($_GET['d'])) header('Content-Type: image/png');
