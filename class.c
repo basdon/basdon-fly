7691a0df787f7001f4f94bef7dde175cb53038ca
yugecin <yugecin@users.noreply.github.com>
2019-10-19 13:12:13 +0200

    simplify(?) class selection code

--- class.c
+++ class.c
@@ -64,27 +64,6 @@ void class_on_player_connect(AMX *amx, int playerid)
 	nc_params[1] = playerid;
 	nc_params[2] = 0x888888FF;
 	NC(n_SetPlayerColor);
-
-	/*This doesn't work on first invocation (which normally happens in
-	class_on_player_request_class), so execute it here once so the animation
-	can 'preload' and works on request class.*/
-	nc_params[2] = SPECIAL_ACTION_DANCE1;
-	NC(n_SetPlayerSpecialAction);
-
-	nc_params[0] = 4;
-	/*position behind the vinewood sign, to hide during login*/
-	*((float*) (nc_params + 2)) = 1415.386f;
-	*((float*) (nc_params + 3)) = -807.9211f;
-	*((float*) (nc_params + 4)) = 85.0615f;
-	NC(n_SetPlayerPos_);
-	*((float*) (nc_params + 2)) = VINEWOOD_CAM_POS_X;
-	*((float*) (nc_params + 3)) = VINEWOOD_CAM_POS_Y;
-	*((float*) (nc_params + 4)) = VINEWOOD_CAM_POS_Z;
-	NC(n_SetPlayerCameraPos);
-	*((float*) (nc_params + 2)) = VINEWOOD_CAM_LAT_X;
-	*((float*) (nc_params + 3)) = VINEWOOD_CAM_LAT_Y;
-	*((float*) (nc_params + 4)) = VINEWOOD_CAM_LAT_Z;
-	NC(n_SetPlayerCameraLookAt);
 }
 
 /**
@@ -102,27 +81,8 @@ void class_on_player_request_class(AMX *amx, int playerid, int classid)
 		playerclass[playerid] = classid;
 	}
 
-	/*This is also called when the player is connecting, but then they
-	aren't logged in yet, so ignore it.*/
-	PC_REMOVEME_isplaying(playerid);
-	if (!pc_result) {
-		return;
-	}
-
-	nc_params[0] = 2;
-	nc_params[1] = playerid;
-	nc_params[2] = CLASS_COLORS[classid];
-	NC(n_SetPlayerColor);
-
-	/*facing angle only looks good when the dancing animation is applied*/
-	*((float*) (nc_params + 2)) = 236.0f;
-	NC(n_SetPlayerFacingAngle);
-
 	nc_params[0] = 4;
-	*((float*) (nc_params + 2)) = 1486.2727f;
-	*((float*) (nc_params + 3)) = -874.0833f;
-	*((float*) (nc_params + 4)) = 58.8885f;
-	NC(n_SetPlayerPos_);
+	nc_params[1] = playerid;
 	*((float*) (nc_params + 2)) = VINEWOOD_CAM_POS_X;
 	*((float*) (nc_params + 3)) = VINEWOOD_CAM_POS_Y;
 	*((float*) (nc_params + 4)) = VINEWOOD_CAM_POS_Z;
@@ -131,16 +91,49 @@ void class_on_player_request_class(AMX *amx, int playerid, int classid)
 	*((float*) (nc_params + 3)) = VINEWOOD_CAM_LAT_Y;
 	*((float*) (nc_params + 4)) = VINEWOOD_CAM_LAT_Z;
 	NC(n_SetPlayerCameraLookAt);
+
+	/*This is also called when the player is connecting, but then they
+	aren't logged in yet, so SetPlayerPos to hide the player and don't
+	do class thingies.*/
+	PC_REMOVEME_isplaying(playerid);
+	if (!pc_result) {
+		/*position behind the vinewood sign, to hide during login*/
+		*((float*) (nc_params + 2)) = 1415.386f;
+		*((float*) (nc_params + 3)) = -807.9211f;
+		*((float*) (nc_params + 4)) = 85.0615f;
+	} else {
+		*((float*) (nc_params + 2)) = 1486.2727f;
+		*((float*) (nc_params + 3)) = -874.0833f;
+		*((float*) (nc_params + 4)) = 58.8885f;
+	}
+	NC(n_SetPlayerPos_);
+
+	/*special action should be done _after_ setting position*/
+	/*Note: while this shouldn't be done when the player has not logged in
+	yet, calling this doesn't work on first invocation (which normally
+	would happens in class_on_player_request_class), so execute it here
+	already so the animation can 'preload' and works on request class.*/
+	nc_params[0] = 2;
+	nc_params[2] = SPECIAL_ACTION_DANCE1;
+	NC(n_SetPlayerSpecialAction);
+
+	if (!pc_result) {
+		/*don't do classy thingies when not logged in*/
+		return;
+	}
+
+	nc_params[2] = CLASS_COLORS[classid];
+	NC(n_SetPlayerColor);
+
+	/*facing angle only looks good when the dancing animation is applied*/
+	*((float*) (nc_params + 2)) = 236.0f;
+	NC(n_SetPlayerFacingAngle);
+
 	amx_SetUString(buf32, CLASS_NAMES[classid], 32);
 	nc_params[2] = buf32a;
 	nc_params[3] = 0x800000;
 	nc_params[4] = 3;
 	NC(n_GameTextForPlayer);
-
-	/*special action should be done _after_ setting position*/
-	nc_params[0] = 2;
-	nc_params[2] = SPECIAL_ACTION_DANCE1;
-	NC(n_SetPlayerSpecialAction);
 }
 
 int class_on_player_request_spawn(AMX *amx, int playerid)
