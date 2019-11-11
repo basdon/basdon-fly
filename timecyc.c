d5646cdbd7b0b50c161e8a78357923acefa8369c
yugecin <yugecin@users.noreply.github.com>
2019-11-11 22:04:11 +0100

    respect line length in timecyc docs

--- timecyc.c
+++ timecyc.c
@@ -18,36 +18,57 @@ WIND wind
 HH   heathaze
 WAVE waveyness
 
-+-----+-----------------------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
-| id  | name                  | VDIS  | CC    | FOGY  | FOGD  | RAIN  | SAND  | WIND  | HH    | WAVE  |
-+-----+-----------------------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
-|  0  | LA Extra Sunny        | 800   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 1.0   | 0.3   |
-|  1  | LA Sunny              | 800   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.25  | 1.0   | 0.55  |
-|  2  | LA Extra Sunny Smog   | 800   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 1.0   | 0.3   |
-|  3  | LA Sunny Smog         | 800   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.2   | 1.0   | 0.5   |
-|  4  | LA Cloudy             | 700   | 1.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.7   | 0.0   | 1.0   |
-|                                                                                                     |
-|  5  | SF Sunny              | 455   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.25  | 0.0   | 0.55  |
-|  6  | SF Extra Sunny        | 455   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.3   |
-|  7  | SF Cloudy             | 1150  | 1.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.7   | 0.0   | 1.0   |
-|  8  | SF Rainy              | 650   | 1.0   | 0.0   | 0.18  | 1.0   | 0.0   | 1.0   | 0.0   | 1.0   |
-|  9  | SF Foggy              | 250   | 1.0   | 1.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.3   |
-|                                                                                                     |
-| 10  | LV Sunny              | 1000  | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.2   | 0.0   | 0.5   |
-| 11  | LV Extra Sunny        | 1000  | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 1.0   | 0.3   |
-| 12  | LV Cloudy             | 1000  | 1.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.4   | 0.0   | 0.7   |
-|                                                                                                     |
-| 13  | CS Extra Sunny        | 1500  | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.3   |
-| 14  | CS Sunny              | 1500  | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.3   | 0.0   | 0.6   |
-| 15  | CS Cloudy             | 1150  | 1.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.7   | 0.0   | 1.0   |
-| 16  | CS Rainy              | 650   | 1.0   | 0.0   | 0.2   | 1.0   | 0.0   | 1.0   | 0.0   | 1.0   |
-|                                                                                                     |
-| 17  | DE Extra Sunny        | 1500  | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 1.0   | 0.3   |
-| 18  | DE Sunny              | 1500  | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.3   | 1.0   | 0.6   |
-| 19  | DE Sandstorm          | 150   | 1.0   | 1.0   | 0.0   | 0.0   | 1.0   | 1.5   | 0.0   | 1.0   |
-|                                                                                                     |
-| 20  | Underwater            | 300   | 1.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.3   |
-+-----+-----------------------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
++-----+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+| id  | VDIS  | CC    | FOGY  | FOGD  | RAIN  | SAND  | WIND  | HH    | WAVE  |
++-----+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+|  0  | LA Extra Sunny                                                        |
+      | 800   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 1.0   | 0.3   |
+|  1  | LA Sunny                                                              |
+|     | 800   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.25  | 1.0   | 0.55  |
+|  2  | LA Extra Sunny Smog                                                   |
+|     | 800   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 1.0   | 0.3   |
+|  3  | LA Sunny Smog                                                         |
+|     | 800   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.2   | 1.0   | 0.5   |
+|  4  | LA Cloudy                                                             |
+|     | 700   | 1.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.7   | 0.0   | 1.0   |
+|                                                                             |
+|  5  | SF Sunny                                                              |
+|     | 455   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.25  | 0.0   | 0.55  |
+|  6  | SF Extra Sunny                                                        |
+|     | 455   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.3   |
+|  7  | SF Cloudy                                                             |
+|     | 1150  | 1.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.7   | 0.0   | 1.0   |
+|  8  | SF Rainy                                                              |
+|     | 650   | 1.0   | 0.0   | 0.18  | 1.0   | 0.0   | 1.0   | 0.0   | 1.0   |
+|  9  | SF Foggy                                                              |
+|     | 250   | 1.0   | 1.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.3   |
+|                                                                             |
+| 10  | LV Sunny                                                              |
+|     | 1000  | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.2   | 0.0   | 0.5   |
+| 11  | LV Extra Sunny                                                        |
+|     | 1000  | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 1.0   | 0.3   |
+| 12  | LV Cloudy                                                             |
+|     | 1000  | 1.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.4   | 0.0   | 0.7   |
+|                                                                             |
+| 13  | CS Extra Sunny                                                        |
+|     | 1500  | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.3   |
+| 14  | CS Sunny                                                              |
+|     | 1500  | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.3   | 0.0   | 0.6   |
+| 15  | CS Cloudy                                                             |
+|     | 1150  | 1.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.7   | 0.0   | 1.0   |
+| 16  | CS Rainy                                                              |
+|     | 650   | 1.0   | 0.0   | 0.2   | 1.0   | 0.0   | 1.0   | 0.0   | 1.0   |
+|                                                                             |
+| 17  | DE Extra Sunny                                                        |
+|     | 1500  | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 1.0   | 0.3   |
+| 18  | DE Sunny                                                              |
+|     | 1500  | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.3   | 1.0   | 0.6   |
+| 19  | DE Sandstorm                                                          |
+|     | 150   | 1.0   | 1.0   | 0.0   | 0.0   | 1.0   | 1.5   | 0.0   | 1.0   |
+|                                                                             |
+| 20  | Underwater                                                            |
+|     | 300   | 1.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.0   | 0.3   |
++-----+-----------------------+-------+-------+-------+-------+-------+-------+
 
 0: clear skies
 1: light clouds
@@ -91,41 +112,51 @@ weighted:
 wind * 34kts
 
  // from sa-mp forums: https://forum.sa-mp.com/showthread.php?t=285077
- 0: METAR INFO: [Current Weather]: Sunny Skies |[Visibility]:High |[Winds]:NA |[Clouds]:Moderate
- 1: METAR INFO: [Current Weather]: Moderate Sunny Skies [Visibility]:High [Winds]:NA [Clouds]:Moderate
- 2: METAR INFO: [Current Weather]: Sunny Skies [Visibility]:High [Winds]:NA [Clouds]:Moderate
- 3: METAR INFO: [Current Weather]: Sunny Skies [Visibility]:High [Winds]:NA [Clouds]:Moderate
- 4: METAR INFO: [Current Weather]: Sunny Skies |[Visibility]:High |[Winds]:NA |[Clouds]:Moderate
- 5: METAR INFO: [Current Weather]: Sunny Skies |[Visibility]:High |[Winds]:NA |[Clouds]:Moderate
+ 0: Sunny Skies |[Visibility]:High |[Winds]:NA |[Clouds]:Moderate
+ 1: Moderate Sunny Skies [Visibility]:High [Winds]:NA [Clouds]:Moderate
+ 2: Sunny Skies [Visibility]:High [Winds]:NA [Clouds]:Moderate
+ 3: Sunny Skies [Visibility]:High [Winds]:NA [Clouds]:Moderate
+ 4: Sunny Skies |[Visibility]:High |[Winds]:NA |[Clouds]:Moderate
+ 5: Sunny Skies |[Visibility]:High |[Winds]:NA |[Clouds]:Moderate
  6:
- 7: METAR INFO: [Current Weather]: Sunny Skies |[Visibility]:High |[Winds]:NA |[Clouds]:Moderate
- 8: METAR INFO: [Current Weather]: Wet Rainy Weather |[Visibility]:Medium |[Winds]:Moderate |[Clouds]:Heavy
- 9: METAR INFO: [Current Weather]: Thick Fog |[Visibility]:Low |[Winds]:Moderate |[Clouds]:Heavy
-10: METAR INFO: [Current Weather]: Moderate Sunny Skies |[Visibility]:High |[Winds]:NA |[Clouds]:Moderate
-11: METAR INFO: [Current Weather]: HeatWave |[Visibility]:High |[Winds]:NA |[Clouds]:Moderate
-12: METAR INFO: [Current Weather]: Hazy/Dull Weather |[Visibility]:Moderate |[Winds]:NA |[Clouds]:High
-13: METAR INFO: [Current Weather]: Hazy/Dull Weather |[Visibility]:Moderate |[Winds]:NA |[Clouds]:High
-14: METAR INFO: [Current Weather]: Hazy/Dull Weather |[Visibility]:Moderate |[Winds]:NA |[Clouds]:High
-15: METAR INFO: [Current Weather]: Heavy RainStorm |[Visibility]:Low |[Winds]:Very High |[Clouds]: very Thick
-16: METAR INFO: [Current Weather]: Scorching Hot Bright Weather |[Visibility]:High |[Winds]:NA |[Clouds]:Low
-17: METAR INFO: [Current Weather]: Scorching Hot Bright Weather |[Visibility]:High |[Winds]:NA |[Clouds]:Low
-18: METAR INFO: [Current Weather]: Scorching Hot Bright Weather |[Visibility]:High |[Winds]:NA |[Clouds]:Low
-19: METAR INFO: [Current Weather]: Sand Storm |[Visibility]:Very-Low |[Winds]: High Speed Winds |[Clouds]:Heavy
-20: METAR INFO: [Current Weather]: Toxic Green Smog |[Visibility]:Low |[Winds]:Moderate |[Clouds]:Heavy
+ 7: Sunny Skies |[Visibility]:High |[Winds]:NA |[Clouds]:Moderate
+ 8: Wet Rainy Weather |[Visibility]:Medium |[Winds]:Moderate |[Clouds]:Heavy
+ 9: Thick Fog |[Visibility]:Low |[Winds]:Moderate |[Clouds]:Heavy
+10: Moderate Sunny Skies |[Visibility]:High |[Winds]:NA |[Clouds]:Moderate
+11: HeatWave |[Visibility]:High |[Winds]:NA |[Clouds]:Moderate
+12: Hazy/Dull Weather |[Visibility]:Moderate |[Winds]:NA |[Clouds]:High
+13: Hazy/Dull Weather |[Visibility]:Moderate |[Winds]:NA |[Clouds]:High
+14: Hazy/Dull Weather |[Visibility]:Moderate |[Winds]:NA |[Clouds]:High
+15: Heavy RainStorm |[Visibility]:Low |[Winds]:Very High |[Clouds]: very Thick
+16: Scorching Hot Bright Weather |[Visibility]:High |[Winds]:NA |[Clouds]:Low
+17: Scorching Hot Bright Weather |[Visibility]:High |[Winds]:NA |[Clouds]:Low
+18: Scorching Hot Bright Weather |[Visibility]:High |[Winds]:NA |[Clouds]:Low
+19: Sand Storm |[Visibility]:Very-Low |[Winds]: High Speed |[Clouds]:Heavy
+20: Toxic Green Smog |[Visibility]:Low |[Winds]:Moderate |[Clouds]:Heavy
 
 weather table 0x8D5EB0
 
-CS | 13, 13, 13, 13, 13, 13, 13, 13, 14, 15,  9,  9, 15, 14, 13, 14, 14, 13, 13, 13, 13, 14, 14, 13, 13
-LA |  2,  2,  0,  0,  0,  2,  2,  2,  3,  0,  3,  2,  0,  0,  3,  3,  3,  4,  4,  0,  0,  1,  1,  0,  2
-SF |  5,  5,  5,  5,  5,  7,  7,  9,  9,  7,  5,  5,  5,  5,  5,  7,  7,  7,  8,  8,  7,  7,  5,  5,  5
-LV | 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, 11
-DE | 17, 17, 17, 19, 19, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 17
+CS | 13, 13, 13, 13, 13, 13, 13, 13, 14, 15,  9,  9, 15,
+     14, 13, 14, 14, 13, 13, 13, 13, 14, 14, 13, 13
+LA |  2,  2,  0,  0,  0,  2,  2,  2,  3,  0,  3,  2,  0,
+      0,  3,  3,  3,  4,  4,  0,  0,  1,  1,  0,  2
+SF |  5,  5,  5,  5,  5,  7,  7,  9,  9,  7,  5,  5,  5,
+      5,  5,  7,  7,  7,  8,  8,  7,  7,  5,  5,  5
+LV | 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+     11, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, 11
+DE | 17, 17, 17, 19, 19, 17, 17, 17, 17, 17, 17, 17, 17,
+     17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 17
 
-CS | 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 15,  9,  9, 15, 16, 16, 16, 16, 15, 14
-LA |  2,  2,  2,  2,  2,  2,  2,  2,  2,  3,  3,  3,  3,  3,  3,  2,  2,  3,  3,  0,  0, 16,  0,  4,  1
-SF |  5,  6,  6,  5,  7,  9,  9,  7,  5,  5,  7,  5,  5,  6,  6,  5,  5,  5,  7,  7,  8,  8,  7,  7,  9
-LV | 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11
-DE | 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 19, 19, 19, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17
+CS | 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, 14, 14, 14,
+     14, 14, 15,  9,  9, 15, 16, 16, 16, 16, 15, 14
+LA |  2,  2,  2,  2,  2,  2,  2,  2,  2,  3,  3,  3,  3,
+      3,  3,  2,  2,  3,  3,  0,  0, 16,  0,  4,  1
+SF |  5,  6,  6,  5,  7,  9,  9,  7,  5,  5,  7,  5,  5,
+      6,  6,  5,  5,  5,  7,  7,  8,  8,  7,  7,  9
+LV | 11, 11, 11, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11,
+     10, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11
+DE | 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 19,
+     19, 19, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17
 
 CS | 14, 14, 14, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13
 LA |  1,  1,  1,  0,  0,  2,  2,  2,  2,  2,  2,  2,  2,  2
@@ -214,7 +245,8 @@ static const unsigned char wavesmapping[WEATHERS] = {
 	/*   0 - 0.3 low */
 	/* 0.4 - 0.6 moderate */
 	/* 0.7 - 1.0 high */
-	24, 10, 24, 10, 5, 10, 24, 5, 5, 24, 10, 24, 5, 24, 10, 5, 5, 24, 10, 5, 24
+	24, 10, 24, 10, 5, 10, 24, 5, 5, 24, 10,
+	24, 5, 24, 10, 5, 5, 24, 10, 5, 24
 };
 
 static unsigned char currentweather = WEATHER_INVALID;
