72eb53607a6e9ad8395eafd7c4739d22b2d2bf7c
yugecin <yugecin@users.noreply.github.com>
2019-12-09 22:41:17 +0100

    order trac tickets: keep resolved and closed ones at the bottom

--- www/trac.php
+++ www/trac.php
@@ -4,7 +4,7 @@ include('../templates/trac_constants.php');
 
 $page = get_page();
 $db_querycount += 2;
-$tracthreads = $db->query('SELECT id,updated,status,severity,summary FROM tract WHERE visibility&'.$usergroups.' OR op='.$userid.' ORDER BY id DESC LIMIT 50 OFFSET '.(($page - 1) * 50));
+$tracthreads = $db->query('SELECT id,updated,status,severity,summary FROM tract WHERE visibility&'.$usergroups.' OR op='.$userid.' ORDER BY status<4 DESC, id DESC LIMIT 50 OFFSET '.(($page - 1) * 50));
 $totalrows = $db->query('SELECT COUNT(id) c FROM tract');
 if ($totalrows && ($totalrows = $totalrows->fetchAll())) {
 	$totalrows = $totalrows[0]->c;
