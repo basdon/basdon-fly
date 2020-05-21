a1007245fb8ed2882a2b2acf84a98c40ed2726cf
yugecin <yugecin@users.noreply.github.com>
2020-05-21 17:20:03 +0200

    Insert some new articles

---
+++ 202005211719-airport-articles.sql
@@ -0,0 +1,3 @@
+INSERT INTO art(cat,name,title) VALUES (1, 'Score', 'Score');
+INSERT INTO art(cat,name,title) VALUES (0, 'San_Fierro_Carrier', 'San Fierro Carrier');
+INSERT INTO art(cat,name,title) VALUES (2, 'Article_Formatting', 'Article Formatting');
