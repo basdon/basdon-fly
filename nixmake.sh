c45d7455f3e8031bc64fa9ceaa07fcd46ffe12db
yugecin <yugecin@users.noreply.github.com>
2020-09-01 01:35:44 +0200

    Readd accidentally removed -Wall compile flag

--- nixmake.sh
+++ nixmake.sh
@@ -5,7 +5,7 @@ rm out/*
 echo BASDON_EXTRA_COMPILATION_OPTIONS=$BASDON_EXTRA_COMPILATION_OPTIONS
 
 (nasm -f elf32 samphost.asm -o out/samphost.o &&
-gcc -m32 -std=c89 $BASDON_EXTRA_COMPILATION_OPTIONS -x c -c basdonfly.c -o out/basdonfly.o &&
+gcc -Wall -m32 -std=c89 $BASDON_EXTRA_COMPILATION_OPTIONS -x c -c basdonfly.c -o out/basdonfly.o &&
 #ld -m elf_i386 -shared --no-undefined -o out/basdonfly.so out/samphost.o out/basdonfly.o) ||
-gcc -m32 -shared -lm -Xlinker --no-undefined -o out/basdonfly.so out/samphost.o out/basdonfly.o) ||
+gcc -Wall -m32 -shared -lm -Xlinker --no-undefined -o out/basdonfly.so out/samphost.o out/basdonfly.o) ||
 (echo "" && echo "" && echo "FAILURE")
