02ebf99f069caa2dffe40fbbda69848dac01c84f
yugecin <yugecin@users.noreply.github.com>
2021-04-11 13:19:33 +0200

    Fix changelog typo again

---
+++ fly-db/20210411131700-fix-changelog-typo.sql
@@ -0,0 +1,3 @@
+UPDATE chg
+SET entry='Fixed not getting a weather bonus if the weather is already bad when starting a mission'
+WHERE entry='Fix not getting a weather bonus if the weather is already bad when starting a mission';
