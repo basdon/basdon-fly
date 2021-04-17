3a3ec32e4b687011b19b8b37f820c200b7f60822
yugecin <yugecin@users.noreply.github.com>
2021-04-17 11:46:54 +0200

    Normalize changelog entries for map additions

---
+++ fly-db/20210417114400-normalize-changelog.sql
@@ -0,0 +1,7 @@
+UPDATE chg
+SET entry='New destination: Iguazu City (IGZU)'
+WHERE entry='Added Iguazu City map/destination';
+
+UPDATE chg
+SET entry='New destination: BNS Atlantic (BNSA) (military)'
+WHERE entry='Added BNS Atlantic map/destination (military)';
