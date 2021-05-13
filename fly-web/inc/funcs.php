0957d3a85fa83f8ae35e999faf0edfd7141fc022
yugecin <yugecin@users.noreply.github.com>
2021-05-13 15:37:03 +0200

    web: allowlist int/float/string in GET/POST params

--- fly-web/inc/funcs.php
+++ fly-web/inc/funcs.php
@@ -1,5 +1,17 @@
 <?php
 
+function sanitize_params(&$pars)
+{
+	foreach ($pars as $k => $v) {
+		if (!is_int($v) && !is_string($v) && !is_float($v)) {
+			unset($pars[$k]);
+		}
+	}
+}
+sanitize_params($_GET);
+sanitize_params($_POST);
+// $_FILES?
+
 function header_value($value)
 {
 	return explode("\r", explode("\n", $value)[0])[0];
