6564312e4810f55abb2bcd4e83da1a3f50ed5344
yugecin <yugecin@users.noreply.github.com>
2021-05-13 15:07:27 +0200

    web: check logoutkey when logging out
    The logout key was generated and put in each logout key, but it
    simply wasn't checked in the logout code itself.
    
    Co-Authored-By: Sunpy <emily@osufx.com>

--- fly-web/www/logout.php
+++ fly-web/www/logout.php
@@ -1,7 +1,7 @@
 <?php
 include('../inc/bootstrap.php');
 
-if (isset($loggeduser)) {
+if (isset($loggeduser, $_GET['k']) && $_GET['k'] === $loggeduser->logoutkey) {
 	setcookie($COOKIENAME, '', 0, $COOKIEPATH, $COOKIEDOMAIN, $COOKIEHTTPS, true);
 	++$db_querycount;
 	$db->query('UPDATE webses SET lastupdate=UNIX_TIMESTAMP() WHERE id=\''.$__sesid.'\'');
