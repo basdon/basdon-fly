027b1b2af4c3d5ca55b5895ec58db808c8901ce7
yugecin <yugecin@users.noreply.github.com>
2021-07-09 00:27:53 +0200

    Fix segfault when running -O2 compiled plugin

--- fly-plugin/raknet.c
+++ fly-plugin/raknet.c
@@ -7,6 +7,7 @@ int raknet_get_messages_in_send_buffer_for_player(int playerid)
 {
 	register struct RakNetStatistics *stats;
 
+	abort(); /*need to fix the commented line in B_OnPlayerConnect, it segfaults when compiled with -O2*/
 	stats = rakServer->vtable->GetStatistics(rakServer, rakPlayerID[playerid]);
 	return stats->messageSendBufferPrior0 +
 		stats->messageSendBufferPrior1 +
