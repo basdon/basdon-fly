4b44dfc243f29b0554cbea819f7614f843b38096
yugecin <yugecin@users.noreply.github.com>
2023-02-11 22:12:29 +0100

    adjust fmt_money colors for WCAG AA accessibility

--- fly-web/inc/funcs.php
+++ fly-web/inc/funcs.php
@@ -29,10 +29,10 @@ function fmt_money($amount)
 {
 	$s = '<span style="color:#';
 	if ($amount < 0) {
-		$s .= 'c22">-$';
+		$s .= 'a22">-$';
 		$amount = -$amount;
 	} else {
-		$s .= '2a2">$';
+		$s .= '282">$';
 	}
 	return $s. number_format($amount, 0, '', ',') . '</span>';
 }
