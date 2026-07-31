basdon/basdon-fly

commit ecb5e63a938c00b5b1a357bd4f35134fbfd77c33

simplify dealing with spawninfo

fly-plugin/samp.c
@@ -147,31 +147,12 @@ void SetGameModeText(char *gamemodetext)
 }
 
 static
-void SetSpawnInfo(int playerid, struct SpawnInfo *spawnInfo)
-{
-	TRACE;
-	struct RPCDATA_SetSpawnInfo03DL rpcdata03DL;
-	struct RPCDATA_SetSpawnInfo037 rpcdata037;
-
-	if (is_player_using_client_version_DL[playerid]) {
-		convertSpawnInfoToSpawnInfo03DL(spawnInfo, &rpcdata03DL.spawnInfo);
-		SendRPC(playerid, RPC_SetSpawnInfo, &rpcdata03DL, sizeof(rpcdata03DL) * 8);
-	} else {
-		rpcdata037.spawnInfo = *spawnInfo;
-		SendRPC(playerid, RPC_SetSpawnInfo, &rpcdata037, sizeof(rpcdata037) * 8);
-	}
-
-	sampPlayer[playerid]->spawnInfo = *spawnInfo;
-	sampPlayer[playerid]->hasSpawnInfo = 1; /*otherwise SAMP will ignore client Spawn packets and player will not be marked (not broadcasted) as spawned despited being spawned*/
-}
-
-static
 void SetPlayerSkin(int playerid, int skin)
 {
 	TRACE;
 	struct RPCDATA_SetPlayerSkin03DL rpcdata03DL;
 	struct RPCDATA_SetPlayerSkin037 rpcdata037;
-	struct BitStream bs03DL, bs037;
+	struct BitStream bs03DL, bs037, *bs[2] = { &bs037, &bs03DL };
 	struct SampPlayer *player;
 	int i;
 
@@ -196,11 +177,7 @@ void SetPlayerSkin(int playerid, int skin)
 
 	for (i = playerpool->highestUsedPlayerid; i >= 0; i--) {
 		if (i == playerid || (sampPlayer[i] && sampPlayer[i]->playerStreamedIn[playerid])) {
-			if (is_player_using_client_version_DL[i]) {
-				SendRPC_bs(i, RPC_SetPlayerSkin, &bs03DL);
-			} else {
-				SendRPC_bs(i, RPC_SetPlayerSkin, &bs037);
-			}
+			SendRPC_bs(i, RPC_SetPlayerSkin, bs[is_player_using_client_version_DL[i]]);
 		}
 	}
 }
@@ -394,6 +371,7 @@ void SpawnPlayer(int playerid)
 
 	rpcdata.type = 2;
 	SendRPC(playerid, RPC_RequestSpawn, &rpcdata, sizeof(rpcdata) * 8);
+	sampPlayer[playerid]->isAllowedToSpawn = 1;
 }
 
 /**
@@ -1420,6 +1398,26 @@ void SendGameTimeUpdate(ushort playerid)
 	rpcdata.time = samp_GetTime();
 	SendRPC(playerid, RPC_GameTimeUpdate, &rpcdata, sizeof(rpcdata) * 8);
 }
+
+static
+void SyncSpawnInfo(ushort playerid)
+{
+	TRACE;
+	struct RPCDATA_SetSpawnInfo03DL rpcdata03DL;
+	register struct SampPlayer *player = sampPlayer[playerid];
+
+	if (is_player_using_client_version_DL[playerid]) {
+		convertSpawnInfoToSpawnInfo03DL(&player->spawnInfo, &rpcdata03DL.spawnInfo);
+		SendRPC(playerid, RPC_SetSpawnInfo, &rpcdata03DL, sizeof(rpcdata03DL) * 8);
+	} else {
+		EXPECT_SIZE(struct RPCDATA_SetSpawnInfo037, sizeof(player->spawnInfo));
+		SendRPC(playerid, RPC_SetSpawnInfo, &player->spawnInfo, sizeof(player->spawnInfo) * 8);
+	}
+}
+
+/*We don't use SetSpawnInfo, write spawn info directly to spawnInfo in player's struct and call SyncSpawnInfo.*/
+void SetSpawnInfo();
+
 #endif
 
 /*-----------------------------------------------------------------------------*/
@@ -1685,14 +1683,13 @@ void natives_SpawnPlayer(int playerid)
 #ifdef SAMP_NATIVES_IMPL
 {
 	TRACE;
-	struct SpawnInfo spawnInfo;
 
 	/*eject player first if they're in a vehicle*/
 	if (GetPlayerVehicleID(playerid)) {
 		ClearAnimations(playerid);
 	}
-	spawn_get_random_spawn(playerid, &spawnInfo);
-	SetSpawnInfo(playerid, &spawnInfo);
+	spawn_get_random_spawn(classid[playerid], &sampPlayer[playerid]->spawnInfo);
+	SyncSpawnInfo(playerid);
 	SpawnPlayer(playerid);
 }
 #endif

fly-plugin/basdonfly.c
@@ -349,7 +349,7 @@ static int spawned[MAX_PLAYERS];
 static char client_inited[MAX_PLAYERS];
 
 /**set in samp.asm*/
-char is_player_using_client_version_DL[MAX_PLAYERS];
+uchar is_player_using_client_version_DL[MAX_PLAYERS];
 /**set in samp.asm*/
 int player_netgame_version[MAX_PLAYERS];
 

fly-plugin/basdonfly_callbacks.c
@@ -243,7 +243,6 @@ static
 cell AMX_NATIVE_CALL B_OnPlayerDeath(AMX *amx, cell *params)
 {
 	TRACE;
-	struct SpawnInfo spawnInfo;
 	struct vec3 pos;
 	const int playerid = PARAM(1), killerid = PARAM(2);
 	const int reason = PARAM(3);
@@ -269,8 +268,8 @@ cell AMX_NATIVE_CALL B_OnPlayerDeath(AMX *amx, cell *params)
 	/*set player spawninfo to a random spawn.
 	 *doing this in OnPlayerDeath allows the client to immediately spawn at the new (random) spawn location,
 	 *as opposed to using SetPosition in OnPlayerSpawn, which would cause an extra teleport that may cause possible map/zone streaming headaches*/
-	spawn_get_random_spawn(playerid, &spawnInfo);
-	SetSpawnInfo(playerid, &spawnInfo);
+	spawn_get_random_spawn(classid[playerid], &sampPlayer[playerid]->spawnInfo);
+	SyncSpawnInfo(playerid);
 
 	timecyc_on_player_death(playerid);
 	GetPlayerPos(playerid, &pos);

fly-plugin/samp_incoming.c
@@ -16,14 +16,9 @@ void HandleRpcSpawned(struct RakRPCHandlerArg *arg)
 		return;
 	}
 	player = playerpool->players[playerid];
-	if (!player->hasSpawnInfo) {
-		/*TODO remove this (we should set default spawn info when player connects & isAllowedToSpawn should prevent this anyways)*/
-		return;
-	}
-	if (player->spawnInfo.skin > 319) {
-		/*TODO remove this (this value should never been set to an invalid value, same as above)*/
-		return;
-	}
+
+	/*TODO: we can get rid of this, as we set default spawn info on connect so we don't care if clients*/
+	/*bypass RequestSpawn or whatever.*/
 	if (!player->isAllowedToSpawn) {
 		/*Getting here means the player is manually sending packets to circumvent initial class selection,*/
 		/*because passing this condition cannot happen with an unmodified client.*/
@@ -98,7 +93,6 @@ void HandleRpcRequestClass(struct RakRPCHandlerArg *arg)
 	struct RPCDATA_RequestClass03DL rpcdata03DL;
 	struct RPCDATA_RequestClass037 rpcdata037;
 	ushort playerid;
-	ushort classid;
 
 	playerid = rakServerVtable->GetIndexFromPlayerID(rakServer, arg->playerID);
 	if (
@@ -109,26 +103,19 @@ void HandleRpcRequestClass(struct RakRPCHandlerArg *arg)
 		return;
 	}
 
-	classid = *((ushort*) arg->rpcdata);
-	/*not clamping classid here, class code will deal with it*/
-
-	class_on_player_request_class(playerid, classid);
+	class_on_player_request_class(playerid, *((ushort*) arg->rpcdata));
 	timecyc_on_player_request_class(playerid);
 
-	spawn_get_random_spawn(playerid, &rpcdata037.spawnInfo);
+	spawn_get_random_spawn(classid[playerid], &sampPlayer[playerid]->spawnInfo);
 	if (is_player_using_client_version_DL[playerid]) {
 		rpcdata03DL.response = 1; /*this is the result of the OnPlayerRequestClass callback*/
-		convertSpawnInfoToSpawnInfo03DL(&rpcdata037.spawnInfo, &rpcdata03DL.spawnInfo);
+		convertSpawnInfoToSpawnInfo03DL(&sampPlayer[playerid]->spawnInfo, &rpcdata03DL.spawnInfo);
 		SendRPC_ex(playerid, RPC_RequestClass, &rpcdata03DL, sizeof(rpcdata03DL) * 8, HIGH_PRIORITY, RELIABLE, 0);
 	} else {
 		rpcdata037.response = 1; /*this is the result of the OnPlayerRequestClass callback*/
+		rpcdata037.spawnInfo = sampPlayer[playerid]->spawnInfo;
 		SendRPC_ex(playerid, RPC_RequestClass, &rpcdata037, sizeof(rpcdata037) * 8, HIGH_PRIORITY, RELIABLE, 0);
 	}
-
-	sampPlayer[playerid]->spawnInfo = rpcdata037.spawnInfo;
-	/*TODO: replace this, still needed now because SAMP will ignore client Spawn packets and player will not be*/
-	/*marked (not broadcasted) as spawned despited being spawned*/
-	sampPlayer[playerid]->hasSpawnInfo = 1;
 }
 /*jeanine:p:i:42;p:23;a:r;x:305.00;y:-25.00;n:HandleRpcRequestSpawn;*/
 static
@@ -150,7 +137,7 @@ void HandleRpcRequestSpawn(struct RakRPCHandlerArg *arg)
 
 	class_on_player_request_spawn(playerid);
 
-	sampPlayer[playerid]->isAllowedToSpawn = 1; /*otherwise Spawned RPC gets ignored*/
+	sampPlayer[playerid]->isAllowedToSpawn = 1;
 	rpcdata.type = 1; /*result of OnPlayerRequestSpawn*/
 	SendRPC_ex(playerid, RPC_RequestSpawn, &rpcdata, sizeof(rpcdata) * 8, HIGH_PRIORITY, RELIABLE, 0);
 }
@@ -479,6 +466,7 @@ void HandleRpcClientJoin(struct RakRPCHandlerArg *arg)
 	}
 	player->worldTime = 720.0f; /*hour*60+minute; 720 = 12:00*/
 	player->_pad2911 = 1;
+	spawn_get_random_spawn(0, &player->spawnInfo);
 	/*skipping allocating textdrawpool/textlabelpool/pool2CDA*/
 	/*below here is code of SampPlayerPool::AddPlayer*/
 	playerjoinrpc.base.playerid = playerid;
@@ -551,7 +539,6 @@ void HandleRpcClientJoin(struct RakRPCHandlerArg *arg)
 
 	/*Here we can use all the functions that SAMP wiki says do not work reliably in OnPlayerConnect,*/
 	/*because now GameInit has been sent so they should work reliably at this point.*/
-
 }
 /*jeanine:p:i:12;p:8;a:r;x:18.00;y:14.00;n:VehicleMarkOccupied;*/
 static

fly-plugin/spawn.c
@@ -103,23 +103,22 @@ void spawn_cb_dlg_spawn(int playerid, struct DIALOG_RESPONSE response)
 }
 
 static
-void spawn_get_random_spawn(int playerid, struct SpawnInfo *outSpawnInfo)
+void spawn_get_random_spawn(int classid, struct SpawnInfo *outSpawnInfo)
 {
 	TRACE;
 	int spawnidx = 0;
-	int klass = classid[playerid];
 
-	switch (numspawns[klass]) {
+	switch (numspawns[classid]) {
 	/*if no spawns, take first spawn of pilot class*/
-	case 0: klass = 0;
+	case 0: classid = 0;
 	case 1: spawnidx = 0; break;
-	default: spawnidx = amxrandom(numspawns[klass]); break;
+	default: spawnidx = amxrandom(numspawns[classid]); break;
 	}
 
 	outSpawnInfo->team = 11; /*using anything but NO_TEAM should make players not able to damage other players, except by slitting throat when having a knife*/
-	outSpawnInfo->skin = CLASS_SKINS[klass];
+	outSpawnInfo->skin = CLASS_SKINS[classid];
 	outSpawnInfo->_pad5 = 69;
-	memcpy(&outSpawnInfo->pos, spawns[klass] + spawnidx, sizeof(struct vec4));
+	outSpawnInfo->pos = spawns[classid][spawnidx];
 	outSpawnInfo->weapon[0] = WEAPON_CAMERA;
 	outSpawnInfo->weapon[1] = 0;
 	outSpawnInfo->weapon[2] = 0;

commit 7886dbfb06d53da4ad47b13a334a2306589707ad

fix SetPlayerSkin not working
broken by ecb5e63a938c00b5b1a357bd4f35134fbfd77c33

fly-plugin/samp.c
@@ -161,11 +161,7 @@ void SetPlayerSkin(int playerid, int skin)
 		return;
 	}
 
-	/*this is what SAMP does*/
-	if (!player->hasSpawnInfo) {
-		player->spawnInfo.skin = skin;
-		return;
-	}
+	player->spawnInfo.skin = skin;
 
 	rpcdata03DL.playerid = rpcdata037.playerid = playerid;
 	rpcdata03DL.skin = rpcdata037.skin = skin;